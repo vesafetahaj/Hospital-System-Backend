b85b8e264778ba05e09ce6ae293b3332363e1a0c
elsatomaj <elsatomaj173@gmail.com>
2023-06-05 16:03:11 +0200

    Contaci edhe dashboardi i Pacientit

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_System_Management.Controllers
+{
+    [Authorize(Roles ="Patient")]
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+
+        {
+
+            return View();
+        }
+        public IActionResult Rezervimi()
+        {
+            return View();
+        }
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+        public IActionResult Raporti()
+        {
+            return View();
+        }
+    }
+}
