7592ceea8abb4ed3a7c9676172e16164c2ed17fc
vesafetahaj <vf58160@ubt-uni.net>
2023-06-05 21:32:57 +0200

    Bug fixes ne pergjithesi

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_System_Management.Models
+{
+    public class ContactFormModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+        public DateTime DateSubmitted { get; set; }
+    }
+}
