dabca18fcf71b5046c56cab0e2efade0e6c0db76
vesafetahaj <vf58160@ubt-uni.net>
2023-06-05 22:10:47 +0200

    Roles CRUD te Dashboard i Admin - done

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -31,5 +31,83 @@ namespace Hospital_System_Management.Controllers
             }
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var role = _manager.FindByIdAsync(id).GetAwaiter().GetResult();
+            if (role != null)
+            {
+                return View(role);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingRole = _manager.FindByIdAsync(role.Id).GetAwaiter().GetResult();
+                if (existingRole != null)
+                {
+                    existingRole.Name = role.Name;
+                    var result = _manager.UpdateAsync(existingRole).GetAwaiter().GetResult();
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Role not found.");
+                }
+            }
+
+            return View(role);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            var role = _manager.FindByIdAsync(id).GetAwaiter().GetResult();
+            if (role != null)
+            {
+                return View(role);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var role = _manager.FindByIdAsync(id).GetAwaiter().GetResult();
+            if (role != null)
+            {
+                var result = _manager.DeleteAsync(role).GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
+  
