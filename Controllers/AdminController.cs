8d2decc54024d07e8db408d0ea24eee78f9f08c0
vesafetahaj <vf58160@ubt-uni.net>
2023-06-07 13:08:27 +0200

    Bug fixes because files got overrided

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,19 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Hospital_System_Management.Data;
+using Hospital_System_Management.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hospital_System_Management.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Contact()
-        {
-            return View();
-        }
+
         public IActionResult Doctors()
         {
             return View();
@@ -26,6 +32,74 @@ namespace Hospital_System_Management.Controllers
         {
             return View();
         }
-     
+        // GET: /Admin/Contact
+        public ActionResult Contact()
+        {
+            var submissions = _context.ContactForms.ToList();
+            return View(submissions);
+        }
+        [HttpGet]
+        public IActionResult EditContact(int id)
+        {
+            var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == id);
+            if (contactForm == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactForm);
+        }
+
+        [HttpPost]
+        public IActionResult EditContact(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == model.Id);
+                if (contactForm == null)
+                {
+                    return NotFound();
+                }
+
+                contactForm.FirstName = model.FirstName;
+                contactForm.LastName = model.LastName;
+                contactForm.Email = model.Email;
+                contactForm.Phone = model.Phone;
+                contactForm.Message = model.Message;
+
+                _context.SaveChanges();
+                return RedirectToAction("Contact");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteContact(int id)
+        {
+            var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == id);
+            if (contactForm == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactForm);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == id);
+            if (contactForm == null)
+            {
+                return NotFound();
+            }
+
+            _context.ContactForms.Remove(contactForm);
+            _context.SaveChanges();
+            return RedirectToAction("Contact");
+        }
+
     }
+
 }
--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -20,6 +20,12 @@ namespace Hospital_System_Management.Controllers
             var contactForms = _context.ContactForms.ToList();
             return View(contactForms);
         }
+        [HttpGet]
+        public IActionResult CreateSuccess()
+        {
+            return View();
+        }
+
 
         [HttpGet]
         public IActionResult Create()
@@ -35,72 +41,12 @@ namespace Hospital_System_Management.Controllers
                 model.DateSubmitted = DateTime.Now;
                 _context.ContactForms.Add(model);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("CreateSuccess");
             }
 
             return View(model);
         }
 
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == id);
-            if (contactForm == null)
-            {
-                return NotFound();
-            }
-
-            return View(contactForm);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(ContactFormModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == model.Id);
-                if (contactForm == null)
-                {
-                    return NotFound();
-                }
-
-                contactForm.FirstName = model.FirstName;
-                contactForm.LastName = model.LastName;
-                contactForm.Email = model.Email;
-                contactForm.Phone = model.Phone;
-                contactForm.Message = model.Message;
-
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult Delete(int id)
-        {
-            var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == id);
-            if (contactForm == null)
-            {
-                return NotFound();
-            }
-
-            return View(contactForm);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var contactForm = _context.ContactForms.FirstOrDefault(c => c.Id == id);
-            if (contactForm == null)
-            {
-                return NotFound();
-            }
-
-            _context.ContactForms.Remove(contactForm);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
     }
+
 }
