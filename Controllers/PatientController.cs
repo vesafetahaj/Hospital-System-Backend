25ddee684b07995518c04e83c09277ec7048fecc
elsatomaj <elsatomaj173@gmail.com>
2023-06-10 01:23:37 +0200

    crudi per termine online-i paperfunduar

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Hospital_System_Management.Data;
+using Hospital_System_Management.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Hospital_System_Management.Controllers
 {
-    [Authorize(Roles ="Patient")]
+    [Authorize(Roles = "Patient")]
     public class PatientController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
 
+        public PatientController(ApplicationDbContext context)
         {
-
+            _context = context;
+        }
+        public IActionResult Index()
+        {
             return View();
         }
+
+
         public IActionResult Rezervimi()
         {
             return View();
@@ -25,9 +35,107 @@ namespace Hospital_System_Management.Controllers
             return View();
         }
 
-        public IActionResult YourAppointment()
+   
+        // Reservation CRUD
+        public ActionResult YourAppointment()
+        {
+            var submissions = _context.OnlineReservation.ToList();
+            return View(submissions);
+        }
+        [HttpGet]
+        public IActionResult EditReservation(int id)
+        {
+            var reserve = _context.OnlineReservation.FirstOrDefault(c => c.Id == id);
+            if (reserve == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserve);
+        }
+
+        [HttpPost]
+        public IActionResult EditReservation(ReservationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var reserve = _context.OnlineReservation.FirstOrDefault(c => c.Id == model.Id);
+                if (reserve == null)
+                {
+                    return NotFound();
+                }
+                reserve.Name = model.Name;
+                reserve.Surname = model.Surname;
+                reserve.Mosha = model.Mosha;
+                reserve.IDCard = model.IDCard;
+                reserve.Sherbimi = model.Sherbimi;
+                reserve.DateSubmitted = model.DateSubmitted;
+
+                _context.SaveChanges();
+                return RedirectToAction("YourAppointment");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteReservation(int id)
+        {
+            var reserve = _context.OnlineReservation.FirstOrDefault(c => c.Id == id);
+            if (reserve == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserve);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var reserve = _context.OnlineReservation.FirstOrDefault(c => c.Id == id);
+            if (reserve == null)
+            {
+                return NotFound();
+            }
+
+            _context.OnlineReservation.Remove(reserve);
+            _context.SaveChanges();
+            return RedirectToAction("YourAppointment");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ReservationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.DateSubmitted = DateTime.Now;
+                _context.OnlineReservation.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("CreateSuccessO");
+            }
+
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Success()
+        {
+            var reserve = _context.OnlineReservation.ToList();
+            return View(reserve);
+        }
+        [HttpGet]
+
+        public IActionResult CreateSuccessO()
         {
             return View();
         }
     }
+
 }
+
+
---
+++ Models/ReservationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital_System_Management.Models
+{
+
+    public class ReservationModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Mosha { get; set; }
+        public string IDCard { get; set; }
+
+        public string Sherbimi { get; set; }
+        public DateTime DateSubmitted { get; set; }
+    }
+}
